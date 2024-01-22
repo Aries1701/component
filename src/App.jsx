9a8c7b5d0a8de665d79b1fd7e0744a5b9e236a6f
Aries1701 <ddnghi1234@gmail.com>
2024-01-22 21:12:15 +0700

    bt s4

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,36 @@
 
 import './App.css'
 import Button from './Button'
-function App() {
+import React, { useState } from 'react';
+
+const App = () => {
+  const [formData, setFormData] = useState({ name: '', age: '', createdAt: new Date() });
+  const [showAgeError, setShowAgeError] = useState(false);
+
+  const handleClick = () => {
+    const name = prompt('Enter your name:');
+    const age = prompt('Enter your age:');
+
+    if (name) {
+      if (age) {
+        setFormData({ name, age, createdAt: new Date() });
+        setShowAgeError(false);
+      } else {
+        setShowAgeError(true);
+      }
+    }
+  };
 
   return (
     <div>
-    <div className='btn1'>
-      <Button text={"Button 1"} border ="solid 1px black" width = "120px" height= "40px" textAlign = "center" paddingTop = "20px" backgroundColor= "#f5f5f5"/>
-      <Button text={"Button 2"} border ="solid 1px black" width = "120px" height= "40px" textAlign = "center" paddingTop = "20px" backgroundColor= "#f5f5f5"/>
-      <Button text={"Button 3"} border ="solid 1px black" width = "120px" height= "40px" textAlign = "center" paddingTop = "20px" backgroundColor= "#f5f5f5"/>
-    </div>
-      <div className='ground'>
-      <div className='btn2'>
-        <Button color="white" border ="solid 1px black" borderRadius="5px" text=" Button 1" backgroundColor="blue" width="200px" height="40px" textAlign="center" paddingTop="20px" />
-        <Button color="white" border ="solid 1px black" borderRadius="5px" text="Button 2" backgroundColor="green" width="200px" height="40px" textAlign="center" paddingTop="20px" />
-        <Button color="white" border ="solid 1px black" borderRadius="5px" text="Button 3" backgroundColor="orange" width="200px" height="40px" textAlign="center" paddingTop="20px" />
-        <Button color="white" border ="solid 1px black" borderRadius="5px" text="Button 4" backgroundColor="red" width="200px" height="40px" textAlign="center" paddingTop="20px" />
-      </div>
-      <div className='btn3'>
-      <Button text={"Button 1"} width = "200px" height= "40px" textAlign = "center" paddingTop = "20px" marginBottom ="5px" backgroundColor= "#D3D3D3"/>
-      <Button text={"Button 2"} width = "200px" height= "40px" textAlign = "center" paddingTop = "20px" marginBottom ="5px" color= "#94cc80" borderTop="1px solid #D3D3D3" borderBottom ="1px solid #D3D3D3"/>
-      <Button text={"Button 3"} width = "200px" height= "40px" textAlign = "center" paddingTop = "20px" color="#0080ff"/>
-    </div>
+      <button onClick={handleClick}>Enter</button>
+      <div>
+        <p>Name: {formData.name}</p>
+        <p>Age: {formData.age}</p>
+        {showAgeError && <p>Please enter your age.</p>}
       </div>
     </div>
-    
-  )
-}
+  );
+};
 
-export default App
+export default App;
